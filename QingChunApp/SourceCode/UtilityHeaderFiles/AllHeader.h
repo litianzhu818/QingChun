99ecd16121e833e43c51154d307ad019b720d95e
Peter Lee <peter.lee@afusion.com>
2014-10-14 14:19:49 +0800

    The new project create

---
+++ QingChunApp/SourceCode/UtilityHeaderFiles/AllHeader.h
@@ -0,0 +1,46 @@
+//
+//  AllHeader.h
+//  QingChunAppApp
+//
+//  Created by Peter Lee on 14-4-24.
+//  Copyright (c) 2014年 Peter Lee. All rights reserved.
+//
+
+#ifndef QingChunApp_AllHeader_h
+#define QingChunApp_AllHeader_h
+
+
+#import "DefineValues.h"
+
+#import "JSONKit.h"
+
+//#import "BaseViewController.h"
+//#import "UserConfig.h"
+//#import "SystemConfig.h"
+//
+//#import "AppDelegate.h"
+//#import "ProgressHUD.h"
+//#import "MBProgressHUD.h"
+//#import "UICustomAlertView.h"
+//#import "NotificationType.h"
+//#import "AlertView.h"
+//#import "XMPPWorker.h"
+//#import "RTCWorker.h"
+//#import "DateTimeTools.h"
+
+//
+//#import "UserInfo.h"
+//#import "GroupInfo.h"
+//#import "LoginUserInfo.h"
+//#import "Message.h"
+//
+//#import <XMPPFramework/XMPPFramework.h>
+//
+//#import "FMDB.h"
+//#import "DBManager.h"
+//#import "ChatFileManager.h"
+//
+//#import "NSString+HttpRequestXML.h"
+//#import "NSString+Encryption.h"
+
+#endif
---
+++ QingChunApp/SourceCode/ViewControllers/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  QingChunApp
+//
+//  Created by Peter Lee on 14/10/9.
+//  Copyright (c) 2014年 Peter Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+#ifdef TEST
+    NSLog(peterLee);
+#endif
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
