42a748d7393c75c1584f9caca27a7d44695df026
Peter Lee <peter.lee@afusion.com>
2014-10-23 16:05:49 +0800

    修改了程序的AppDelegate文件

--- QingChunApp/SourceCode/AppDelegate.h
+++ QingChunApp/SourceCode/AppDelegate.h
@@ -8,10 +8,25 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import "NetStatusManager.h"
+
+typedef NS_ENUM(NSInteger, NetworkStatusType){
+    NetworkStatusDefaultType = 0,
+    NetworkStatusNoInternetType
+};
+
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (assign, nonatomic) NetworkStatusType networkStatus;
+/**
+ *  设置系统window的根视图控制器
+ *
+ *  @param viewController 跟视图控制器
+ */
+-(void)setApplicationRootViewController:(UIViewController *)viewController;
 
 //@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 //@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
--- QingChunApp/SourceCode/AppDelegate.m
+++ QingChunApp/SourceCode/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -15,33 +16,270 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     // Override point for customization after application launch.
+    //获取启动信息
+    [self registerRemoteNotificationWith:application];
+    [self launchWitchRemoteNotification:launchOptions];
+    //FIXME: we should not set the applicationIconBadgeNumber of the application to 0 here
+    application.applicationIconBadgeNumber = 0;
+    [self startCheckNetwork];
+//    [self startEngine];
+    [self startReadPlistFiles];
+    [self initData];
+    return YES;
+}
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    //send the deviceToken
+//    NSString* device_token = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+//    [[UserConfig sharedInstance] SetDeviceToken:device_token];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    LOG(@"Push Register Error:%@", error.description);
+}
+
+-(void)launchWitchRemoteNotification:(NSDictionary *)startInfoDic
+{
+    NSDictionary* launchDic = [startInfoDic objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (launchDic){//从远端推送启动的
+        
+    }
+}
+
+//判断启动信息
+//-(BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
+//    if(!url){
+//        return NO;
+//    }
+//    //Kissnapp://
+//    //LOG(@"Calling Application Bundle ID: %@", sourceApplication);
+//    LOG(@"URL scheme:%@", [url scheme]);
+//    LOG(@"URL query: %@", [url query]);
+//    NSString *infoString = [url query];
+//    switch ([infoString integerValue]) {
+//        case 101:
+//            [NotificationCenter postNotificationName:EMAIL_REGISTER_SUCCEED object:infoString];
+//            break;
+//
+//        default:
+//            break;
+//    }
+//    return YES;
+//}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    if(!url){
+        return NO;
+    }
+    //字符串形式：Kissnapp://?XXXXXXXXXX
+    LOG(@"Calling Application Bundle ID: %@\nURL scheme:%@\nURL query: %@", sourceApplication,[url scheme],[url query]);
+    NSString *infoString = [url query];
+    switch ([infoString integerValue]) {
+        case 101:
+            [NotificationCenter postNotificationName:EMAIL_REGISTER_SUCCEED object:infoString];
+            break;
+            
+        default:
+            break;
+    }
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //TODO:处理界面跳转
+    LOG(@"收到远程推送：%@",userInfo.description);
+    
+    NSString* alertStr = nil;
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    NSObject *alert = [apsInfo objectForKey:@"alert"];
+    if ([alert isKindOfClass:[NSString class]]){
+        alertStr = (NSString*)alert;
+    }
+    else if ([alert isKindOfClass:[NSDictionary class]]){
+        NSDictionary* alertDict = (NSDictionary*)alert;
+        alertStr = [alertDict objectForKey:@"body"];
+    }
+    application.applicationIconBadgeNumber = [[apsInfo objectForKey:@"badge"] integerValue];
+    if ([application applicationState] == UIApplicationStateActive && alertStr != nil){
+        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"Pushed Message" message:alertStr delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    //TODO:处理界面跳转
+    LOG(@"收到本地推送：%@,%@",notification.alertBody,notification.userInfo.description);
+    NSDictionary *notificationDic = notification.userInfo;
+    //FIXME:这里需要弹出对话框进行提醒，目前默认同意
+//    if ([[notificationDic objectForKey:@"noticeType"] isEqualToString:@"ADD_FRIEND_REQUEST"]) {
+//        [[[XMPPWorker sharedInstance] xmppRoster] acceptPresenceSubscriptionRequestFrom:[notificationDic objectForKey:@"userID"] withSelfNickName:[notificationDic objectForKey:@"selfNickName"] userName:[notificationDic objectForKey:@"userNickName"] andAddToRoster:YES];
+//    }
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//#if TARGET_IPHONE_SIMULATOR
+//    LOG(@"info:%@&%@",@"The iPhone simulator does not process background network traffic. ",@"Inbound traffic is queued until the keepAliveTimeout:handler: fires.");
+//#endif
+//    
+//    if ([application respondsToSelector:@selector(setKeepAliveTimeout:handler:)])
+//    {
+//        [application setKeepAliveTimeout:600 handler:^{
+//            
+//            LOG(@"KeepAliveHandler");
+//            // Do other keep alive stuff here.
+//        }];
+//    }
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    application.applicationIconBadgeNumber = 0;
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application
+{
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
-    //[self saveContext];
+//    [self stopEngine];
+}
+
+
+/****************************************************初始化数据***********************************************************/
+
+//初始化数据
+- (void)initData
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [self currentViewController];
+    [self.window makeKeyAndVisible];
+}
+
+//初始化网络检查工具
+-(void)startCheckNetwork
+{
+    NetStatusManager *netStatusManager = [NetStatusManager sharedInstance];
+    [netStatusManager setDelegate:self];
+    self.networkStatus = ([netStatusManager getInitNetworkStatus] ? NetworkStatusDefaultType:NetworkStatusNoInternetType);
+}
+////初始化Config对象
+//-(void)startReadConfigFiles
+//{
+//    UserConfig *userConfig = [UserConfig sharedInstance];
+//    SystemConfig *systemConfig = [SystemConfig sharedInstance];
+//}
+
+//读取.plist文件
+-(void)startReadPlistFiles
+{
+    GLOBAL_GCD(^{
+//        NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Systemconfig" ofType:@"plist"];
+//        NSDictionary *systemIbInfoDic = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+//        [[XMPPWorker sharedInstance] setHostName:[systemIbInfoDic objectForKey:@"XMPPServerHost"]];
+//        [[XMPPWorker sharedInstance] setHostPort:[(NSString*)[systemIbInfoDic objectForKey:@"XMPPServerPort"] intValue]];
+//        [[SystemConfig sharedInstance] SetFileUploadURL:[systemIbInfoDic objectForKey:@"FileUploadURL"]];
+//        [[SystemConfig sharedInstance] SetImageCompression:[[systemIbInfoDic objectForKey:@"ImageCompression"] floatValue]];
+    });
+    
+}
+
+////开启XMPP和RTC服务
+//- (void)startEngine
+//{
+//    [[XMPPWorker sharedInstance] startEngine];
+//    [[RTCWorker sharedInstance] startEngine];
+//}
+////关闭XMPP和RTC服务
+//- (void)stopEngine
+//{
+//    [[XMPPWorker sharedInstance] stopEngine];
+//    [[RTCWorker sharedInstance] stopEngine];
+//}
+/*********************************************************************************************************************/
+
+/**
+ *  获取启动时系统的根Viewcontroller
+ *
+ *  @return viewcontroller
+ */
+-(id)currentViewController
+{
+//    if ([[UserConfig sharedInstance] GetAutoLogin]) {
+//        //自动登录并跳转到主页面
+//        MainTabBarController *mainViewController = [self instantiateInitialViewControllerWithStroryboardName:@"Main"];
+//        [mainViewController setNeedLogin:YES];
+//        return mainViewController;
+//    }
+    //跳转到登录页面
+    return [self instantiateInitialViewControllerWithStroryboardName:@"Start"];
+}
+/**
+ *  根据storyboard获取开始的viewcontroller对象
+ *
+ *  @param storyboardName storyboard名称
+ *
+ *  @return storyboard开始的viewcontroller对象
+ */
+-(id)instantiateInitialViewControllerWithStroryboardName:(NSString *)storyboardName
+{
+    return [[UIStoryboard storyboardWithName:storyboardName bundle:nil] instantiateInitialViewController];
+}
+
+//register remote push notification
+-(void)registerRemoteNotificationWith:(UIApplication *)application
+{
+    // Register for push notifications
+    [application registerForRemoteNotificationTypes:
+     UIRemoteNotificationTypeBadge |
+     UIRemoteNotificationTypeAlert |
+     UIRemoteNotificationTypeSound];
+}
+
+-(void)setApplicationRootViewController:(UIViewController *)viewController
+{
+    [self.window setRootViewController:viewController];
+}
+
+#pragma mark -
+#pragma mark CheckNetStatusDelegate Methods
+//运行断网处理
+-(void)DisconnectNetWork
+{
+    LOG(@"Disconnect with the network");
+    self.networkStatus = NetworkStatusNoInternetType;
+}
+//有网时处理
+-(void)ConnectNetWork
+{
+    LOG(@"Connect with the network");
+    self.networkStatus = NetworkStatusDefaultType;
+}
+//没有网络时
+-(void)NoNetWork
+{
+    LOG(@"NO_Network at this time");
+    self.networkStatus = NetworkStatusNoInternetType;
 }
 
 #pragma mark - Private methods
