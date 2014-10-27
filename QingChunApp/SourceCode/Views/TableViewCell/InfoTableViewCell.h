1272a6062c1cd4d1b6bdf33c1aaf8f6573894121
Peter Lee <peter.lee@afusion.com>
2014-10-27 15:05:05 +0800

    怎加了cell

---
+++ QingChunApp/SourceCode/Views/TableViewCell/InfoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  InfoTableViewCell.h
+//  QingChunApp
+//
+//  Created by Peter Lee on 14/10/27.
+//  Copyright (c) 2014年 Peter Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoTableViewCell : UITableViewCell
+
+@end
---
+++ QingChunApp/SourceCode/Views/TableViewCell/InfoTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  InfoTableViewCell.m
+//  QingChunApp
+//
+//  Created by Peter Lee on 14/10/27.
+//  Copyright (c) 2014年 Peter Lee. All rights reserved.
+//
+
+#import "InfoTableViewCell.h"
+
+@implementation InfoTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
