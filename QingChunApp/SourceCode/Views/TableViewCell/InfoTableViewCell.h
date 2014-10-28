f2cbaa6bdf2d481a5d494b2b43be76d631dc2e42
李天柱 <PeterLee@PeterdeMacBook-Pro.local>
2014-10-28 23:06:26 +0800

    调整了cell

--- QingChunApp/SourceCode/Views/TableViewCell/InfoTableViewCell.h
+++ QingChunApp/SourceCode/Views/TableViewCell/InfoTableViewCell.h
@@ -9,5 +9,29 @@
 #import <UIKit/UIKit.h>
 
 @interface InfoTableViewCell : UITableViewCell
+{
+    IBOutlet UIButton *photoButton;
+    IBOutlet UIButton *detailButton;
+    IBOutlet UIButton *zhanButton;
+    IBOutlet UIButton *tuButton;
+    IBOutlet UIButton *shareButton;
+    IBOutlet UIButton *commentButton;
+    
+    IBOutlet UILabel *nameLabel;
+    IBOutlet UILabel *timeLabel;
+    IBOutlet UILabel *textlabel;
+    IBOutlet UIImageView *imageview;
+}
+
+@property (weak, nonatomic) IBOutlet UIButton *photoButton;
+@property (weak, nonatomic) IBOutlet UIButton *detailButton;
+@property (weak, nonatomic) IBOutlet UIButton *zhanButton;
+@property (weak, nonatomic) IBOutlet UIButton *tuButton;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
+@property (weak, nonatomic) IBOutlet UIButton *commentButton;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *textlabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageview;
 
 @end
