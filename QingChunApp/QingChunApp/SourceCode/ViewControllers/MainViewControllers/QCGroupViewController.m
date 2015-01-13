366482393024057521e83c8fdd14f07da76ae089
李天柱 <PeterLee@PeterdeMacBook-Pro.local>
2015-01-14 01:02:47 +0800

    fix  bugs

--- QingChunApp/QingChunApp/SourceCode/ViewControllers/MainViewControllers/QCGroupViewController.m
+++ QingChunApp/QingChunApp/SourceCode/ViewControllers/MainViewControllers/QCGroupViewController.m
@@ -14,14 +14,67 @@
 
 @implementation QCGroupViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self initializationParameters];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)initializationParameters
+{
+    //Here initialization your parameters
+    [self initializationUI];
+    [self initializationData];
+}
+
+-(void)initializationUI
+{
+    //Here initialization your UI parameters
+    self.title = @"青春风铃";
+    
+    _tableView = ({
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, VIEW_BY(self.navigationController.navigationBar), VIEW_W(self.view), VIEW_H(self.view)-VIEW_H(self.navigationController.navigationBar)-VIEW_H(self.tabBarController.tabBar)) style:UITableViewStylePlain];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        [tableView registerClass:[QCBellTableViewCell class] forCellReuseIdentifier:[QCBellTableViewCell CellIdentifier]];
+        
+        if ([tableView respondsToSelector:@selector(setSeparatorInset:)]) {
+            [tableView setSeparatorInset:UIEdgeInsetsZero];
+        }
+        
+        if ([tableView respondsToSelector:@selector(setLayoutMargins:)]) {
+            [tableView setLayoutMargins:UIEdgeInsetsZero];
+        }
+        
+        [self clearUnusedCellWithTableView:tableView];
+        
+        [self.view addSubview:tableView];
+        tableView;
+    });
+}
+
+-(void)initializationData
+{
+    //Here initialization your data parameters
+    _dateSources = [self createDataSources];
+    
 }
 
 /*
