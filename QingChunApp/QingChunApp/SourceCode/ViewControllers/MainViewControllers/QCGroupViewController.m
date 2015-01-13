586e05180c3274102dfa485c3d1a47eb28e412ae
李天柱 <PeterLee@PeterdeMacBook-Pro.local>
2015-01-14 01:01:14 +0800

    怎么加了青友团的界面

---
+++ QingChunApp/QingChunApp/SourceCode/ViewControllers/MainViewControllers/QCGroupViewController.m
@@ -0,0 +1,37 @@
+//
+//  QCGroupViewController.m
+//  QingChunApp
+//
+//  Created by  李天柱 on 15/1/14.
+//  Copyright (c) 2015年 Peter Lee. All rights reserved.
+//
+
+#import "QCGroupViewController.h"
+
+@interface QCGroupViewController ()
+
+@end
+
+@implementation QCGroupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
